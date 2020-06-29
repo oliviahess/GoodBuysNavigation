43b4a16299762dd6020da2cc86c3236ae51a9add
Olivia Hess <oliviahess@olivias-mbp-2.lan>
2020-06-29 14:04:13 -0600

    Initial Commit

---
+++ GoodBuysNavigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoodBuysNavigation
+//
+//  Created by Olivia Hess on 6/29/20.
+//  Copyright © 2020 Olivia Hess. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
